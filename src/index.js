9aecf92282870dff2f3ad069afa77bea0adb255f
AndriiSolodaiev <andik05016@gmail.com>
2023-07-03 18:13:42 +0300

    remove BrowserRouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/main.css";
 import App from "./App";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./i18n";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
