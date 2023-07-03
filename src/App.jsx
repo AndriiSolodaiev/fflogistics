13134d63074f59b1baa825901f65b62fed83f1ac
AndriiSolodaiev <andik05016@gmail.com>
2023-07-03 16:47:53 +0300

    add / to route

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="transportation-requirements" element={<Conditions />} />
+        <Route path="/transportation-requirements" element={<Conditions />} />
         <Route path="delivery-from-Europe" element="DeliveryEurope" />
         <Route path="delivery-from-SthKorea" element="DeliverySthKorea" />
         <Route path="delivery-from-UAE" element="DeliveryUAE" />
@@ -20,7 +20,7 @@ function App() {
         <Route path="sea-transportation" element={<SeaTrans />} />
         <Route path="rail-transportation" element={<RailTrans />} />
         <Route path="blog" element="Blog" />
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<Home />} />
       </Route>
     </Routes>
   );
