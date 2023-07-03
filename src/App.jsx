953f3134980c87af137118dabbf2f08eceee9b8a
AndriiSolodaiev <andik05016@gmail.com>
2023-07-03 16:53:43 +0300

    add jsconfig.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import { Home } from "./pages/Home";
 import { lazy } from "react";
