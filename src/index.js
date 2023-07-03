388c520ab32cb2c9f4fc2ae9358252109c12c6e7
AndriiSolodaiev <andik05016@gmail.com>
2023-07-03 22:07:24 +0300

    BrouserRouter added

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/main.css";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./i18n";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter forceRefresh={true}>
+  <BrowserRouter forceRefresh={true}>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
