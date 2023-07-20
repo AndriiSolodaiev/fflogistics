3b0c8766e014b72e961776bff450916847b2b6e7
AndriiSolodaiev <andik05016@gmail.com>
2023-07-20 17:28:39 +0300

    fix warnings

--- src/pages/Conditions.jsx
+++ src/pages/Conditions.jsx
@@ -1,6 +1,5 @@
 import { useTranslation } from "react-i18next";
 import { ClientForm } from "../components";
-import { imgSrcMaker } from "../helpers/imgSrcMaker";
 
 const Conditions = () => {
   const { t } = useTranslation();
@@ -12,13 +11,7 @@ const Conditions = () => {
       <section className=" conditions__section">
         <div className="container conditions__text-content">
           <p className="conditions__subtitle">{t("conditions.subtitle")}</p>
-          {/* <div className="conditions__bg-img"></div> */}
-          {/* <img
-            src={imgSrcMaker("tablet/conditions-boxes.jpg")}
-            alt="packing example"
-            loading="lazy"
-            className="conditions__img-boxes"
-          /> */}
+
           <ol className="conditions__list">
             {t("conditions.list")
               .split("$")
